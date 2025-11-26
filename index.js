440e0bcac83fbfd5f3caec022873a75e114afd1f
Gilberto Durán <gilbertoduran@MacBook-Pro-de-Gilberto.local>
2025-11-25 21:01:42 -0600

    feat: adding auth as a route

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express';
-import  librosRoutes from './src/routes/libros.routes.js';
+import librosRoutes from './src/routes/libros.routes.js';
+import authRoutes from './src/routes/auth.routes.js';
+import userRoutes from './src/routes/users.routes.js';
+import cors from 'cors';
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.use('/libros', librosRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes); 
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
