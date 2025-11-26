0faddc0561af0ccdf6f7b4e9aded2b4ef624b2bd
Gilberto Durán <gilbertoduran@MacBook-Pro-de-Gilberto.local>
2025-11-25 22:00:21 -0600

    feat: crud usuarios

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users.controller.js";
+import { getUsers, getUserById, updateUser, deactivateUser, activateUser } from "../controllers/users.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { requireRole } from "../middlewares/role.middleware.js";
 
 const router = Router();
 
-router.get("/", 
-  authMiddleware,
-  requireRole("admin"),
-  getUsers
-);
+router.get("/", authMiddleware, requireRole("admin"), getUsers);
 
-export default router;  
+router.get("/:id", authMiddleware, requireRole("admin"), getUserById);
+
+router.put("/:id", authMiddleware, requireRole("admin"), updateUser);
+
+router.delete("/:id", authMiddleware, requireRole("admin"), deactivateUser);
+
+router.put("/:id/activate", authMiddleware, requireRole("admin"), activateUser);
+
+export default router;
